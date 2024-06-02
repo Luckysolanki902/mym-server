27d108bc2e1d5304e453a838d8110531c95b9a02
Lucky <luckysolanki902@gmail.com>
2024-06-02 20:40:36 +0530

    revert changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const socketIO = require('socket.io');
 const cors = require('cors');
 const handleSocketEvents = require('./utils/socketEvents');
-const PairingManager = require('./utils/pairingManager');
+const PairingManager = require('./utils/pairingManger');
 
 const app = express();
 const server = http.createServer(app);
@@ -53,43 +53,8 @@ io.on('connection', (socket) => {
   }
 });
 
-// API endpoint to get user statistics
-app.get('/api/user-stats', (req, res) => {
-  try {
-    const getUsersStats = (usersMap) => {
-      let totalUsers = 0;
-      let maleUsers = 0;
-      let femaleUsers = 0;
-
-      for (let user of usersMap.values()) {
-        totalUsers++;
-        if (user.userGender === 'male') {
-          maleUsers++;
-        } else if (user.userGender === 'female') {
-          femaleUsers++;
-        }
-      }
-
-      return { totalUsers, maleUsers, femaleUsers };
-    };
-
-    const textChatStats = getUsersStats(textChatUsers);
-    const audioCallStats = getUsersStats(audioCallUsers);
-    const videoCallStats = getUsersStats(videoCallUsers);
-
-    res.json({
-      textChatStats,
-      audioCallStats,
-      videoCallStats,
-    });
-  } catch (error) {
-    console.error('Error fetching user stats:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
 server.listen(1000, () => {
-  console.log('Server started on port 1000');
+  console.log('Server started');
 });
 
 process.on('unhandledRejection', (error) => {
